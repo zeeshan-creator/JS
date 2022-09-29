98c73ecfe16be38fa9135eb11554b93e7a457d6a
Zk-sidtechno <112874041+Zk-sidtechno@users.noreply.github.com>
2022-09-29 18:41:36 +0500

    Create add_attributes_after_initializing_dataTables.js

---
+++ add_attributes_after_initializing_dataTables.js
@@ -0,0 +1,26 @@
+$.extend(true, $.fn.dataTable.defaults, {
+            "language": {
+                "decimal": "",
+                "emptyTable": "Non ci sono segnalazioni disponibili",
+                "info": "Mostra _START_ to _END_ of _TOTAL_ segnalazioni",
+                "infoEmpty": "Mostra 0 di 0 di 0 entries",
+                "infoFiltered": "(filtra da _MAX_ tutte le segnalazioni)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostra _MENU_ sengalazioni",
+                "loadingRecords": "Caricamento...",
+                "processing": "Caricamento...",
+                "search": "Cerca:",
+                "zeroRecords": "Nessun Risultato Trovato",
+                "paginate": {
+                    "first": "Prima",
+                    "last": "Ultima",
+                    "next": "Prossima",
+                    "previous": "Precedente"
+                },
+                "aria": {
+                    "sortAscending": ": activate to sort column ascending",
+                    "sortDescending": ": activate to sort column descending"
+                }
+            }
+        });
